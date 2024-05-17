4c8067e449778a2ea2b411d83c62c1b95c7efab8
ArnabRollin <iamarnab.phukan@gmail.com>
2024-05-17 15:19:26 +0530

    Removed cache script.

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::{read_to_string, write},
-    path::Path,
-    process::Command,
-};
+use std::process::Command;
 
 use color_eyre::{
     eyre::{eyre, WrapErr},
@@ -12,22 +8,6 @@ use color_eyre::{
 fn main() -> Result<()> {
     install()?;
 
-    // Cache Script: Start
-
-    let path = Path::new("grammar.cache");
-    let grammar = read_to_string("src/lrparser.lalrpop")
-        .with_context(|| "Could not find grammar `src/lrparser.lalrpop`")?;
-
-    if path.exists() {
-        let contents = read_to_string(path)?;
-
-        if contents == grammar {
-            // return Ok(());
-        }
-    }
-
-    // Cache Script: End
-
     Command::new("sh").arg("-c").arg("cd").arg("src").output()?;
 
     lalrpop::process_root()
@@ -35,9 +15,6 @@ fn main() -> Result<()> {
         .with_context(|| "Could not process all the files in the current directory.")?;
 
     Command::new("sh").arg("-c").arg("cd").arg("..").output()?;
-
-    // Write cache.
-    write("grammar.cache", grammar)?;
 
     Ok(())
 }
